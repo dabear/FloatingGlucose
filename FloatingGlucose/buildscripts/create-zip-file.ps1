795266f8c91f42880e63b28bb8f70472566db53d
Bjorn Inge <bjorninge@gmail.com>
2016-07-28 18:07:06 +0200

    automatically build .zip file of release when building

---
+++ FloatingGlucose/buildscripts/create-zip-file.ps1
@@ -0,0 +1,71 @@
+﻿# Out-Zip.msh 
+# creates out-Zip function 
+# /\/\o\/\/ 2006    
+# http://mow001.blogspot.com 
+
+function out-zip($zipfilename, $files) { 
+      #Load some assemblys. (No line break!)
+    [System.Reflection.Assembly]::Load("WindowsBase,
+       Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")
+
+    #Create a zip file named "MyZipFile.zip". (No line break!)
+    $ZipPackage=[System.IO.Packaging.ZipPackage]::Open("$zipfilename",
+       [System.IO.FileMode]"OpenOrCreate", [System.IO.FileAccess]"ReadWrite")
+
+    #For each file you want to add, we must extract the bytes
+    #and add them to a part of the zip file.
+    ForEach ($file In $files)
+    {
+       $partName=New-Object System.Uri(( "/"+ $file.Name) , [System.UriKind]"Relative")
+       #Create each part. (No line break!)
+       $part=$ZipPackage.CreatePart($partName, "application/zip",
+          [System.IO.Packaging.CompressionOption]"Maximum")
+       $bytes=[System.IO.File]::ReadAllBytes($file)
+       $stream=$part.GetStream()
+       $stream.Write($bytes, 0, $bytes.Length)
+       $stream.Close()
+    }
+
+    #Close the package when we're done.
+    $ZipPackage.Close()
+} 
+
+function getZipFileName() {
+    $projectname = $env:projectname
+    #$projectname = "FloatingGlucose"
+    $log = (git log --pretty=oneline --decorate | select -first 1)
+    #$log = (git log --pretty=oneline --decorate | select -Index 3)
+    $commit_id = "commit-" + ($log.Split(" ") | select -first 1).substring(0,8)
+
+    $changes = (git status -s --porcelain)
+    $has_uncommitted_changes = ($changes.length -ne 0)
+
+    #echo "has changes?"$has_uncommitted_changes
+    #echo "last id:"$commit_id
+
+    if($log -match "\(tag: (?<ver>.*)\)") {
+        #basically, if there is a release associated with this commit, use that release isntead of commit
+        $commit_id = $Matches["ver"]
+    }
+
+    if($has_uncommitted_changes) {
+        $version =  $commit_id+"+changes"
+
+    } else {
+        $version = $commit_id
+
+    }
+    return "$projectname-$version.zip"
+ 
+}
+
+
+
+$zipfilename = getZipFileName
+$dir = pwd
+$outpath = "$dir\$zipfilename"
+
+$files = ls . |  where { -not ($_.name.EndsWith(".vshost.exe") -or $_.name.EndsWith(".zip"))  }
+
+
+$files | out-zip -zipfilename $outpath -files $files
