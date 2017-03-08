efcd970bd2f3b68c729a2ccde0cd1f06ddb6edcb
Bjorn <bjoberg@signicat.com>
2017-03-08 16:51:47 +0100

    simplify dexcomsharenonus module

--- FloatingGlucose/Classes/DataSources/Plugins/DexcomShare/DexcomShareNonUS.cs
+++ FloatingGlucose/Classes/DataSources/Plugins/DexcomShare/DexcomShareNonUS.cs
@@ -19,60 +19,14 @@ namespace FloatingGlucose.Classes.DataSources.Plugins
 {
     internal class DexcomShareNonUSEndpoint : DexcomShareEndpoint
     {
-        protected new ShareClient shareClient = new ShareClient(ShareServer.ServerNonUS);
+        //protected new ShareClient shareClient = new ShareClient(ShareServer.ServerNonUS);
         public override string DataSourceShortName => "Dexcom Share (Non-US)";
 
-        public override void OnPluginSelected(FormGlucoseSettings form)
+        public DexcomShareNonUSEndpoint()
         {
-            form.lblDataSourceLocation.Text = "Dexcom share server";
-            form.txtDataSourceLocation.Text = this.shareClient.CurrentDexcomServer;
+            this.shareClient.SetShareServer(ShareServer.ServerNonUS);
         }
 
-        public override bool VerifyConfig(Properties.Settings settings)
-        {
-            var username = settings.UserName;
-            var password = settings.HashedPassword?.Text ?? "";
-
-            if (username.Length < 2)
-            {
-                throw new ConfigValidationException("UserName field was not correctly filled!");
-            }
-
-            if (password.Length < 1)
-            {
-                throw new ConfigValidationException("Password field was not correctly filled!");
-            }
-
-            shareClient.Username = username;
-            shareClient.Password = password;
-
-            /*if (!Validators.IsReadableFile(settings.DataPathLocation))
-            {
-                throw new ConfigValidationException("You have entered an invalid file path for the data dump!");
-            }*/
-
-            return true;
-        }
-
-        public override async Task<IDataSourcePlugin> GetDataSourceDataAsync(NameValueCollection locations)
-        {
-            try
-            {
-                //this can return null if the internet connection is broken
-
-                this.shareGlucose = await shareClient.FetchLast(3);
-            }
-            catch (SpecificShareError err)
-            {
-                if (err.code == ShareKnownRemoteErrorCodes.AuthenticateAccountNotFound || err.code == ShareKnownRemoteErrorCodes.AuthenticatePasswordInvalid)
-                {
-                    this.shareGlucose = null;
-                    throw new ConfigValidationException($"Dexcom share client uknown username or password. Entered username: {shareClient.Username}");
-
-                }
-            }
-
-            return this;
-        }
+   
     }
 }
